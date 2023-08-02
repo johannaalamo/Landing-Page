3c4a22047f59001530665bb52cb69b6830411bf1
johannaalamo <134504529+johannaalamo@users.noreply.github.com>
2023-08-02 19:40:08 +0000

    Cambios

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Jumbotron = () => {
+  return (
+    
+    <div className="jumbotron">
+      <h1 className="display-4">Welcome to our Landing Page</h1>
+      <p className="lead">This is a simple landing page built with React and Bootstrap components.</p>
+    </div>
+  );
+};
+export default Jumbotron;
